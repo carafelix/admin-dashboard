df42eec38ecf75ee67485a63f596a8f37975e713
Hero Protagonist <heroprotagonist32@gmail.com>
2023-10-21 02:27:11 -0300

    fixed sidebar loading, need to fix monkeys

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -52,29 +52,25 @@ async function skeletonJazz(parentElement:HTMLDivElement){
 
         if(isProject){
 
-            const monkeyImg = populateImg(monkeyFileNames, visitedMonkeys, './assets/dall-e/webp/high/', 'Primate art', 'monkey-img' )
+            const monkeyImg = populateImg(monkeyFileNames, visitedMonkeys, './assets/dall-e/webp/high/', 'Primate art', 'monkey-img', false, false)
 
             if(trueItemDivFromTemplate){
-                removeSkeletonSibilingsOnLoad(monkeyImg, parentElement, '.skeleton');
-                addDisplayRemoveLoadingClass(parentElement, trueItemDivFromTemplate as HTMLDivElement);
+                removeAlreadyLoadedSkeletons(parentElement) // this need to be changed
                 callNextSibiling(parentElement)
             }
             if(h3){
                 trueItemDivFromTemplate?.insertBefore(monkeyImg,h3);
             }
         } else if (isAnnouncement){
-                    removeAlreadyLoadedSkeletons(parentElement)
-                    addDisplayRemoveLoadingClass(parentElement, trueItemDivFromTemplate as HTMLDivElement);
-                    callNextSibiling(parentElement);
+                removeAlreadyLoadedSkeletons(parentElement)
+                callNextSibiling(parentElement);
 
         } else if (isTrend){
 
-            const currentProfile = populateImg(profileNames, visitedProfiles, './assets/profiles/', 'Profile Icon', 'profile-icon')
-                removeSkeletonSibilingsOnLoad(currentProfile, parentElement, '.trend-div');
-                addDisplayRemoveLoadingClass(parentElement, trueItemDivFromTemplate as HTMLDivElement);
-                callNextSibiling(parentElement);
-
+            const currentProfile = populateImg(profileNames, visitedProfiles, './assets/profiles/', 'Profile Icon', 'profile-icon', true);
+                removeSkeletonsOnLoad(currentProfile, parentElement, '.trend-div');
                 parentElement.insertBefore(currentProfile, parentElement.firstElementChild)
+                callNextSibiling(parentElement);
                 
         }
 }
@@ -83,29 +79,39 @@ skeletonJazz(projects[0]);
 skeletonJazz(announcements[0]);
 skeletonJazz(trending[0])
 
-function populateImg(sourceNameList:string[], visited:string[], dir:string, alt:string, imgClass:string){
+function populateImg(sourceNameList:string[], visited:string[], dir:string, alt:string, imgClass:string, absolute?:boolean, hide?:boolean){
     const img = document.createElement('img');
     const current = getCurrentMonkey(sourceNameList, visited);
     const path = dir + current
-
     img.setAttribute('src' , path)
     img.setAttribute('alt' , alt)
     img.classList.add(imgClass)
+    if(absolute){
+        img.classList.add('absolute')
+    }
+    if(hide){
+        img.classList.add('hide')
 
-    console.log(img)
+    }
 
     return img
 }
 
 
-function removeSkeletonSibilingsOnLoad(lastLoadedElement:HTMLElement, parent:HTMLDivElement, selector : string){
+function removeSkeletonsOnLoad(lastLoadedElement:HTMLElement, parent:HTMLElement, selector : string){
 
     lastLoadedElement.addEventListener('load',()=>{
         const skeletonSibilings = parent.querySelectorAll(selector);
         skeletonSibilings.forEach((el:Element)=>{
             parent.removeChild(el)
-        })
+        });
+
+        lastLoadedElement.classList.remove('absolute')
+        parent.querySelectorAll('.true-trend').forEach((el:Element)=>{
+            el.classList.remove('display-none')
+        });
     })
+
 }
 
 function removeAlreadyLoadedSkeletons(parent:HTMLDivElement){
@@ -114,20 +120,13 @@ function removeAlreadyLoadedSkeletons(parent:HTMLDivElement){
         parent.removeChild(el)
     })
 }
-function addDisplayRemoveLoadingClass(parent:HTMLDivElement, template:HTMLDivElement){
-    parent.classList.remove('loading');
-    template?.classList.remove('display-none');
-}
+
 
 function callNextSibiling(parent:HTMLDivElement){
     if(parent.nextElementSibling){
         skeletonJazz(parent.nextElementSibling as HTMLDivElement)
     }
 }
-
-// skeletonJazz(trending[0])
-
-
 
 function getRandomItem(arr:string[]){
     return arr[Math.floor(Math.random()*arr.length)]
